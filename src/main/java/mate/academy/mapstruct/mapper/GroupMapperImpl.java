957803e27486d028036d6f17580411fa77c14876
Egor <llikojia162@gmail.com>
2025-07-31 20:28:47 +0200

    Implemented mappers for Subject, Student and Group

---
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapperImpl.java
@@ -0,0 +1,33 @@
+package mate.academy.mapstruct.mapper;
+
+import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
+import mate.academy.mapstruct.dto.group.GroupDto;
+import mate.academy.mapstruct.model.Group;
+
+public class GroupMapperImpl implements GroupMapper {
+
+    @Override
+    public GroupDto toDto(Group group) {
+        if (group == null) {
+            return null;
+        }
+
+        GroupDto groupDto = new GroupDto();
+        groupDto.setId(group.getId());
+        groupDto.setName(group.getName());
+
+        return groupDto;
+    }
+
+    @Override
+    public Group toModel(CreateGroupRequestDto requestDto) {
+        if (requestDto == null) {
+            return null;
+        }
+
+        Group group = new Group();
+        group.setName(requestDto.name());
+
+        return group;
+    }
+}
